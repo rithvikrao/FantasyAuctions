7e55c008c74ed3c82d4debceff1f930b4e9fd99b
Rithvik Rao <rithvik321@gmail.com>
2019-12-04 18:07:24 -0500

    no moar bugz

--- auctions/snake_draft.py
+++ auctions/snake_draft.py
@@ -12,21 +12,28 @@ def snake_draft(preference_orders, num_rounds):
 	current_round = 0
 
 	allocs = [[] for i in range(len(preference_orders))]
-	allocated = {}
+	allocated = set()
 
 	while current_round < num_rounds:
 		if current_round % 2 == 0:
-			for i in range(len(preference_orders)):
-				for j in range(len(preference_orders[i])):
-					if preference_orders[i][j] not in allocated:
-						allocs[i].append(preference_orders[i][j])
-						allocated.add(preference_orders[i][j])
+			loop_over = range(len(preference_orders))
 		else:
-			for i in range(len(preference_orders) - 1, -1, -1):
-				for j in range(len(preference_orders[i])):
-					if preference_orders[i][j] not in allocated:
-						allocs[i].append(preference_orders[i][j])
-						allocated.add(preference_orders[i][j])
+			loop_over = reversed(range(len(preference_orders)))
+
+		for i in loop_over:
+			for j in range(len(preference_orders[i])):
+				if preference_orders[i][j] not in allocated:
+					allocs[i].append(preference_orders[i][j])
+					allocated.add(preference_orders[i][j])
+					break
+
 		current_round += 1
 
 	return allocs
+
+
+print(snake_draft([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 
+			[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+			[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+			[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
+			[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]], 3))
